cc1a109f02b73992e8a21de215854daff76f580c
networkninjadh <networkninjadh@gmail.com>
2015-10-22 17:59:47 -0500

    launcher fixed

--- Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
+++ Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
@@ -2,9 +2,14 @@ package com.blankoinc.blanko;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class WelcomeActivity extends Activity {
-
+    ImageButton signInButton;
+    ImageButton signUpButton;
+    EditText    email;
+    EditText    pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
