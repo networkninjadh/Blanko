780cae3ff7d160cf0776385e1c5aa08b075ac571
networkninjadh <networkninjadh@gmail.com>
2015-10-22 16:57:07 -0500

    add welcome activity layout

--- Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
+++ Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
@@ -2,8 +2,6 @@ package com.blankoinc.blanko;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class WelcomeActivity extends Activity {
 
@@ -13,25 +11,4 @@ public class WelcomeActivity extends Activity {
         setContentView(R.layout.activity_welcome);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
