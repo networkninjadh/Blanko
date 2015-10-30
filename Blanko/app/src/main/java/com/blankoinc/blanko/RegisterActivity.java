9a8a63f481c90419662cb800fcb749743eb2c507
networkninjadh <networkninjadh@gmail.com>
2015-10-30 01:31:38 -0500

    dialog works for login page orientations added so that turning screen does not change the apearance

--- Blanko/app/src/main/java/com/blankoinc/blanko/RegisterActivity.java
+++ Blanko/app/src/main/java/com/blankoinc/blanko/RegisterActivity.java
@@ -2,13 +2,55 @@ package com.blankoinc.blanko;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
+    ImageButton signUpButton;
+    EditText email, pass, name, robonum;
+    String   emailString, passString, nameString, robonumString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        signUpButton = (ImageButton) findViewById(R.id.sign_up_button);
+        name   = (EditText)findViewById(R.id.Name);
+        email   = (EditText)findViewById(R.id.Email);
+        pass    = (EditText)findViewById(R.id.Pass);
+        robonum   = (EditText)findViewById(R.id.Robonum);
+        emailString = email.getText().toString();
+        nameString = name.getText().toString();
+        passString = pass.getText().toString();
+        robonumString = robonum.getText().toString();
     }
-
+    public boolean checkInput(){
+        boolean inputValid = true;
+        if (emailString == null)
+        {
+            //replace with a custom dialog box
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT);
+            inputValid = false;
+        }
+        if (nameString == null)
+        {
+            //replace with a custom dialog box
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT);
+            inputValid = false;
+        }
+        if (passString == null)
+        {
+            //replace with a custom dialog box
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT);
+            inputValid = false;
+        }
+        if (robonumString == null)
+        {
+            //replace with a custom dialog box
+            Toast.makeText(this, "Please enter the robot number", Toast.LENGTH_SHORT);
+            inputValid = false;
+        }
+        return inputValid;
+    }
 }
--- Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
+++ Blanko/app/src/main/java/com/blankoinc/blanko/WelcomeActivity.java
@@ -1,16 +1,20 @@
 package com.blankoinc.blanko;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WelcomeActivity extends Activity {
-    ImageButton signInButton;
-    ImageButton signUpButton;
-    EditText    email;
-    EditText    pass;
+    ImageButton signInButton, signUpButton;
+    EditText    email, pass;
+    String      emailString, passString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +23,55 @@ public class WelcomeActivity extends Activity {
         signUpButton = (ImageButton) findViewById(R.id.sign_up_button);
         email   = (EditText)findViewById(R.id.Email);
         pass    = (EditText)findViewById(R.id.Pass);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
     }
-    public void signIn()
-    {
-<<<<<<< HEAD
-=======
-        Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
-        startActivity(intent);
->>>>>>> 3922770b6e551d7a26229cdaaa4044c2159ca9a4
+    public void signIn() {
+        checkInput();
     }
 
-    public void signUp()
-    {
+    public void signUp() {
         //starts the register activity
         Intent intent = new Intent(WelcomeActivity.this, RegisterActivity.class);
         startActivity(intent);
+    }
+    public void checkInput(){
+        emailString = email.getText().toString();
+        passString = pass.getText().toString();
+        if (emailString.trim().length() <= 0) // the string is empty
+        {
+           createDialog("Please Enter an Email Address!");
+        }
+        if (passString.trim().length() <= 0) // the string is empty
+        {
+            createDialog("Please Enter a Password!");
+        }
+    }
+    public void createDialog(String message){
+        final Dialog dialog = new Dialog(WelcomeActivity.this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.setTitle("Alert");
+        TextView text = (TextView)dialog.findViewById(R.id.text);
+        text.setText(message);
+        ImageView image = (ImageView)dialog.findViewById(R.id.image);
+        image.setImageResource(R.drawable.ic_launcher);
+        Button dialogButton = (Button)dialog.findViewById(R.id.dialogButtonOK);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
